922feaf96a500fa4e64357060fd3659d5d3f0b7c
Joanne Zanetton <joannezanetton@gmail.com>
2015-06-10 12:02:45 +0100

    first commit

---
+++ js/gitprofile.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('.gitprofile').on('submit', function(e) {
+
+    e.preventDefault();
+
+    var access_token = '77b7d023a6250dd5147de099cd47169f753dba47' // Replace this with a Github access token that you can generate as explained here https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+    var url = 'https://api.github.com/users/' + $('input.username').val() + "?access_token=" + access_token;
+
+    var template = $('template').html();
+
+    $.get(url, function(info) {
+      $('.container').prepend(Mustache.render(template, info));
+    }).fail(function() {
+      $('.container').prepend("User not found")
+    }).always(function() {
+      $('input.username').val('');
+      });
+  });
+});
